45507fd1ffb66ea6ce77c0377b55e579ce835977
Snyp <gabrielmarques.mg@gmail.com>
2020-04-14 14:50:59 -0300

    [Add]_HealthCheck

---
+++ src/test/java/br/ce/gabruw/tasks/health/HealthCheckIT.java
@@ -0,0 +1,38 @@
+package br.ce.gabruw.tasks.health;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class HealthCheckIT {
+
+	@Test
+	public void healthCheck() throws MalformedURLException {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		options.addArguments("disable-infobars");
+		options.addArguments("--disable-extensions");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--no-sandbox");
+
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.108:4444/wd/hub"), options);
+
+		try {
+			driver.navigate().to("http://192.168.0.108:8001/tasks");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+			String text = driver.findElement(By.id("addTodo")).getText();
+			Assert.assertTrue(text.startsWith("Add"));
+		} finally {
+			driver.quit();
+		}
+	}
+}
